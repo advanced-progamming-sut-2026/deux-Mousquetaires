0fa1281627d5ae36f2661627299f31650d4e91d5
Soheil Soleimani <ssoheilss138831@gmail.com>
2026-07-20 22:21:14 +0330

    java25///18

---
+++ Main.java
@@ -0,0 +1,11 @@
+package pvz;
+
+import pvz.controller.GameApp;
+
+public final class Main {
+    private Main() {}
+    /// public static void main(String[] args) {
+    ///         new GameApp().run();
+    ///     }
+    static void main() {new GameApp().run();}
+}
